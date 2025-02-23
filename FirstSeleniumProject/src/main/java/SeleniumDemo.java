096debb8eb189afad1034c0c49a24774a2f9f188
thanhnc <thanhnc279@gmail.com>
2025-02-24 00:08:42 +0700

    use WebElement in Selenium

--- FirstSeleniumProject/src/main/java/SeleniumDemo.java
+++ FirstSeleniumProject/src/main/java/SeleniumDemo.java
@@ -1,14 +1,38 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+
+import java.util.List;
 
 public class SeleniumDemo {
     public static void main(String[] args){
+        /*
         String chromeTitle = chromeTest("https://www.google.com/");
         System.out.println(chromeTitle);
 
         String firefoxTitle = firefoxTest("https://www.facebook.com/");
         System.out.println(firefoxTitle);
+
+         */
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://practicetestautomation.com/practice-test-login/");
+        // WebElement locator will be added here
+        WebElement usernameInputField = driver.findElement(By.id("username"));
+        WebElement passwordInputField = driver.findElement(By.name("password"));
+        WebElement submitButton = driver.findElement(By.className("btn"));
+
+        List<WebElement> inputFields = driver.findElements(By.tagName("input"));
+
+        WebElement linkTextLocator = driver.findElement(By.linkText("Practice Test Automation"));
+        WebElement partialLinkTextLocator = driver.findElement(By.partialLinkText("Test Automation"));
+
+        WebElement passwordFieldBelowUsername = driver.findElement(RelativeLocator.with(By.tagName("input")).below(By.id("username")));
+        WebElement privacyPolicyLink = driver.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(By.partialLinkText("Test Automation")));
+
+        driver.quit();
     }
 
     private static String chromeTest(String url) {
