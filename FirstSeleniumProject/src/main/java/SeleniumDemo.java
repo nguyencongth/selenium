644ad266afc3d66049de3fcc38eab16c639a9d11
thanhnc <thanhnc279@gmail.com>
2025-03-11 00:22:40 +0700

    TestNG

--- FirstSeleniumProject/src/main/java/SeleniumDemo.java
+++ FirstSeleniumProject/src/main/java/SeleniumDemo.java
@@ -3,9 +3,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.locators.RelativeLocator;
-
-import java.util.List;
 
 public class SeleniumDemo {
     public static void main(String[] args){
@@ -18,21 +15,43 @@ public class SeleniumDemo {
 
          */
         WebDriver driver = new ChromeDriver();
+        findLoginPageElements(driver);
+        driver.quit();
+    }
+
+    private static void findExceptionPageElements(WebDriver driver) {
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/");
+    }
+
+    private static void findLoginPageElements(WebDriver driver) {
+
         driver.get("https://practicetestautomation.com/practice-test-login/");
         // WebElement locator will be added here
         WebElement usernameInputField = driver.findElement(By.id("username"));
+        usernameInputField.sendKeys("TestUser");
+        //WebElement usernameInputFieldXpath = driver.findElement(By.xpath("//input[@id='username']"));
+        //WebElement usernameInputFieldCss = driver.findElement(By.cssSelector("input[id='username']"));
+
         WebElement passwordInputField = driver.findElement(By.name("password"));
+        passwordInputField.sendKeys("TestPassword");
+        //WebElement passwordInputFieldXpath = driver.findElement(By.xpath("//input[@name='password']"));
+        //WebElement passwordInputFieldCss = driver.findElement(By.cssSelector("input[name='password']"));
+
         WebElement submitButton = driver.findElement(By.className("btn"));
+        submitButton.isDisplayed();
+        submitButton.click();
+        //WebElement submitButtonXpath = driver.findElement(By.xpath("//button[@id='submit']"));
+        //WebElement submitButtonCss = driver.findElement(By.cssSelector("button[id='submit']"));
 
-        List<WebElement> inputFields = driver.findElements(By.tagName("input"));
+        /*List<WebElement> inputFields = driver.findElements(By.tagName("input"));
 
-        WebElement linkTextLocator = driver.findElement(By.linkText("Practice Test Automation"));
+        WebElement linkTextLocator = driver.findElement(By.linkText("Practice Test Automation."));
         WebElement partialLinkTextLocator = driver.findElement(By.partialLinkText("Test Automation"));
 
         WebElement passwordFieldBelowUsername = driver.findElement(RelativeLocator.with(By.tagName("input")).below(By.id("username")));
-        WebElement privacyPolicyLink = driver.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(By.partialLinkText("Test Automation")));
+        WebElement privacyPolicyLink = driver.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(By.partialLinkText("Test Automation")));*/
 
-        driver.quit();
+
     }
 
     private static String chromeTest(String url) {
